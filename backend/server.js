f81cfeb7e6d5f76f287f7d7122276c8853d5c1c7
Hero-Alpha <yash2909.yk@gmail.com>
2025-09-17 18:45:32 +0530

    fixing deployement issues

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import historyRoutes from "./routes/history.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -28,7 +29,7 @@ async function start() {
   app.use("/api/request", requestRoutes);
   app.use("/api/history", historyRoutes);
 
-  app.listen(5000, () => console.log("Backend running on PORT 5000"));
+  app.listen(PORT, () => console.log(`Backend running on PORT: ${PORT}`));
 }
 
 start().catch((err) => console.error(err));
